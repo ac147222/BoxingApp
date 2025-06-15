5e0013bd38417ad881812cf1dad2162cea5628f9
Farjad Habib <ac147222@avcol.school.nz>
2025-06-15 19:05:48 +1200

    added the AuthenticateUser method in StorageManager.cs to help log in a user which returns null if the users login credentials are wrong

--- Repositories/StorageManager.cs
+++ Repositories/StorageManager.cs
@@ -1,102 +1,100 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Runtime.InteropServices;
 using Microsoft.Data.SqlClient;
-using BoxingDatabase;
 
-namespace BoxingDatabase
+public class StorageManager
 {
-    public class StorageManager
-    {
-        private SqlConnection conn;
-        
-        public StorageManager(string connectionString)
-        {
-            try
-            {
-                conn = new SqlConnection(connectionString);
-                conn.Open();
-                Console.WriteLine("Connection successful");
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine("The connection was unsuccessful");
-                Console.WriteLine(e.Message);
-            }
-            catch (Exception ex) 
-            {
-                Console.WriteLine("An error occurred");
-                Console.WriteLine(ex.Message);
-            }
-        }
+    private SqlConnection conn;
 
-        public List<Region> GetAllRegions()
+    public StorageManager(string connectionString)
+    {
+        conn = new SqlConnection(connectionString);
+        conn.Open();
+    }
+
+    public User AuthenticateUser(string username, string password)
+    {
+        string sql = "SELECT * FROM dbo.tblUser WHERE Username = @Username AND Password = @Password";
+        using (SqlCommand cmd = new SqlCommand(sql, conn))
         {
-            List<Region> regions = new List<Region>();
-            string sqlString = "SELECT * FROM dbo.tblRegion";
-            using (SqlCommand cmd = new SqlCommand(sqlString, conn))
+            cmd.Parameters.AddWithValue("@Username", username);
+            cmd.Parameters.AddWithValue("@Password", password);
+            using (SqlDataReader reader = cmd.ExecuteReader())
             {
-                using (SqlDataReader reader = cmd.ExecuteReader())
+                if (reader.Read())
                 {
-                    while (reader.Read())
+                    return new User
                     {
-                        int Region_ID = Convert.ToInt32(reader["RegionID"]);
-                        string Region_Name = reader["RegionName"].ToString();
-                        regions.Add(new Region(Region_ID, Region_Name));
-                    }
+                        UserID = (int)reader["UserID"],
+                        Username = reader["Username"].ToString(),
+                        Role = reader["Role"].ToString(),
+                        FullName = reader["FullName"].ToString(),
+                        
+                    };
                 }
             }
-            return regions;
         }
+        return null;
+    }
 
-        public void UpdateRegionName(int regionId, string newName)
-        {
-            string sqlString = "UPDATE dbo.tblRegion SET RegionName = @RegionName WHERE RegionID = @RegionID";
-            using (SqlCommand cmd = new SqlCommand(sqlString, conn))
-            {
-                cmd.Parameters.AddWithValue("@RegionID", regionId);
-                cmd.Parameters.AddWithValue("@RegionName", newName);
-                int rowsAffected = cmd.ExecuteNonQuery();
-                if (rowsAffected > 0)
-                    Console.WriteLine("Region updated successfully.");
-                else
-                    Console.WriteLine("Region not found.");
-            }
-        } 
 
-        public void InsertRegion(string regionName)
+    public List<Region> GetAllRegions()
+    {
+        List<Region> regions = new List<Region>();
+        string sqlString = "SELECT * FROM dbo.tblRegion";
+        using (SqlCommand cmd = new SqlCommand(sqlString, conn))
         {
-            string sqlString = "INSERT INTO dbo.tblRegion (RegionName) VALUES (@RegionName)";
-            using (SqlCommand cmd = new SqlCommand(sqlString, conn))
+            using (SqlDataReader reader = cmd.ExecuteReader())
             {
-                cmd.Parameters.AddWithValue("@RegionName", regionName);
-                cmd.ExecuteNonQuery();
-            }
-            Console.WriteLine("Region added successfully.");
-        }
-
-        public void DeleteRegionByName(string regionName)
-        {
-            string sqlString = "DELETE FROM dbo.tblRegion WHERE RegionName = @RegionName";
-            using (SqlCommand cmd = new SqlCommand(sqlString, conn))
-            {
-                cmd.Parameters.AddWithValue("@RegionName", regionName);
-                int rowsAffected = cmd.ExecuteNonQuery();
-                if (rowsAffected > 0)
-                    Console.WriteLine("Region deleted successfully.");  
-                else
-                    Console.WriteLine("Region not found.");
+                while (reader.Read())
+                {
+                    int Region_ID = Convert.ToInt32(reader["RegionID"]);
+                    string Region_Name = reader["RegionName"].ToString();
+                    regions.Add(new Region(Region_ID, Region_Name));
+                }
             }
         }
+        return regions;
+    }
 
-        public void CloseConnection()
+    public void InsertRegion(string regionName)
+    {
+        string sqlString = "INSERT INTO dbo.tblRegion (RegionName) VALUES (@RegionName)";
+        using (SqlCommand cmd = new SqlCommand(sqlString, conn))
         {
-            if (conn != null && conn.State == ConnectionState.Open)
-            {
-                conn.Close();
-                Console.WriteLine("Connection closed");
-            }
+            cmd.Parameters.AddWithValue("@RegionName", regionName);
+            cmd.ExecuteNonQuery();
+        }
+        Console.WriteLine("Region added successfully.");
+    }
+
+    public void UpdateRegionName(int regionId, string newName)
+    {
+        string sqlString = "UPDATE dbo.tblRegion SET RegionName = @RegionName WHERE RegionID = @RegionID";
+        using (SqlCommand cmd = new SqlCommand(sqlString, conn))
+        {
+            cmd.Parameters.AddWithValue("@RegionID", regionId);
+            cmd.Parameters.AddWithValue("@RegionName", newName);
+            int rowsAffected = cmd.ExecuteNonQuery();
+            if (rowsAffected > 0)
+                Console.WriteLine("Region updated successfully.");
+            else
+                Console.WriteLine("Region not found.");
+        }
+    }
+
+    public void DeleteRegionByName(string regionName)
+    {
+        string sqlString = "DELETE FROM dbo.tblRegion WHERE RegionName = @RegionName";
+        using (SqlCommand cmd = new SqlCommand(sqlString, conn))
+        {
+            cmd.Parameters.AddWithValue("@RegionName", regionName);
+            int rowsAffected = cmd.ExecuteNonQuery();
+            if (rowsAffected > 0)
+                Console.WriteLine("Region deleted successfully.");  
+            else
+                Console.WriteLine("Region not found.");
         }
     }
 }
