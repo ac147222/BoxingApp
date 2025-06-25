6cff0cff1568d62c21ab271f9a84acd566a410d6
Farjad Habib <ac147222@avcol.school.nz>
2025-06-25 12:26:12 +1200

    Fixed my error by creating a new constructor for Weightclasses in the weightclasses.cs file

--- Model/Weightclasses.cs
+++ Model/Weightclasses.cs
@@ -8,11 +8,18 @@ namespace BoxingApp
     {
         public string Weightclasses_ID { get; set; }
         public string Weightclasses_Name { get; set; }
+        public int Weightclasses_ID1 { get; }
 
         public Weightclasses(string WeightclassesID, string WeightclassesName)
         {
             Weightclasses_ID = WeightclassesID;  
             Weightclasses_Name = WeightclassesName;
+        }
+
+        public Weightclasses(int weightclasses_ID, string? weightclasses_Name)
+        {
+            Weightclasses_ID1 = weightclasses_ID;
+            Weightclasses_Name = weightclasses_Name;
         }
     }
 }
