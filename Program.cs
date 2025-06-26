db387feee70f7d850ee8fa37d9225b8d8e568afc
Farjad Habib <ac147222@avcol.school.nz>
2025-06-27 09:55:33 +1200

    fixed my error where the mian menu was glitching by removing a null value for a console.readline, and changing my switch case inot a loop in my program.cs file

--- Program.cs
+++ Program.cs
@@ -5,24 +5,26 @@ using System.Collections.Generic;
 string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=\"C:\\USERS\\FARJA\\ONEDRIVE - AVONDALE COLLEGE\\FARJADBOXINGDATABASE\\BOXINGAPP\\DB\\BOXINGDATABASE.MDF\";Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 StorageManager storageManager = new StorageManager(connectionString);
 
-
-while (true)
+bool loop = true;
+do
 {
     
     Console.WriteLine("\nRegion Menu:\n1. View Regions\n2. Insert Region\n3. Update Region\n4. Delete Region\n5. Exit");
 
-    string choice = null;
+    string choice = Console.ReadLine();
     switch (choice)
     {
         case "1":
             List<Region> regions = storageManager.GetAllRegions();
             foreach (var r in regions)
                 Console.WriteLine($"{r.Region_ID}: {r.Region_Name}");
+            loop =false;
             break;
         case "2":
             Console.Write("Enter new region name: ");
             string name = Console.ReadLine();
             storageManager.InsertRegion(name);
+            loop = false;
             break;
         case "3":
             Console.Write("Enter region ID to update: ");
@@ -30,16 +32,20 @@ while (true)
             Console.Write("Enter new name: ");
             string newName = Console.ReadLine();
             storageManager.UpdateRegionName(rid, newName);
+            loop = false;
             break;
         case "4":
             Console.Write("Enter region name to delete: ");
             string delName = Console.ReadLine();
             storageManager.DeleteRegionByName(delName);
+            loop = false;
             break;
         case "5":
+            loop = false;
             return;
         default:
             Console.WriteLine("Invalid option.");
+            loop = true;    
             break;
     }
-}
+} while (loop);
